2cadcec4f56aef5b8d25ae92bb14fa8d6730f9c1
RWBRO <ofccristian28@gmail.com>
2026-09-10 12:13:20 -0300

    Add files via upload

--- src/main/java/br/VapeVlite/ModuleManager.java
+++ src/main/java/br/VapeVlite/ModuleManager.java
@@ -15,10 +15,20 @@ public class ModuleManager {
         modules.add(new JumpReset());
         modules.add(new Backtrack());
         modules.add(new Reach());
+        modules.add(new Velocity());
+        modules.add(new Hitbox());
+        modules.add(new Criticals());
+        modules.add(new WTap());
+        modules.add(new KillAura());
         modules.add(new NoJumpDelay());
         modules.add(new NoHitDelay());
         modules.add(new FastPlace());
         modules.add(new AutoTool());
+        modules.add(new InventoryMove());
+        modules.add(new SafeWalk());
+        modules.add(new Fly());
+        modules.add(new NoFall());
+        modules.add(new AutoSoup());
     }
 
     public List<Module> getModules() { return modules; }
