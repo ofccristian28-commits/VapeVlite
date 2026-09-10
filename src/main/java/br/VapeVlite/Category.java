eeda184b2b9b97703faedb8ad2342e55da29af1d
RWBRO <ofccristian28@gmail.com>
2026-09-09 23:48:15 -0300

    Add files via upload

--- src/main/java/br/VapeVlite/Category.java
+++ src/main/java/br/VapeVlite/Category.java
@@ -1,5 +1,20 @@
 package br.vapevlite;
 
 public enum Category {
-    COMBAT
+    PLAYER("player"),
+    RENDER("render"),
+    COMBAT("combat"),
+    WORLD("world"),
+    MOVEMENT("movement"),
+    UTILS("utils");
+
+    private final String displayName;
+
+    Category(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
