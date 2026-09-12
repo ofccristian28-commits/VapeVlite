7cf3e01ce01fbaf993a6fc8040c8ef4db7b8fbb8
cristian <ofccristian28@gmail.com>
2026-09-12 09:06:24 -0300

    Fix BooleanValue syntax

--- src/main/java/me/vene/skilled/values/BooleanValue.java
+++ src/main/java/me/vene/skilled/values/BooleanValue.java
@@ -1,30 +1,33 @@
- package me.vene.skilled.values;
+package me.vene.skilled.values;
 
- import me.vene.skilled.utilities.StringRegistry;
- 
+import me.vene.skilled.utilities.StringRegistry;
+
 public class BooleanValue
 {
     private String name;
     private boolean value;
-    
+
     public BooleanValue(final String name, final boolean value) {
         this.name = StringRegistry.register(name);
         this.value = value;
     }
-    
+
     public String getName() {
         return StringRegistry.register(this.name);
     }
-    
+
     public void setName(final String name) {
         this.name = name;
     }
-    
+
     public boolean getState() {
-      public void setState(final boolean value) { this.value = value; }
         return this.value;
     }
-    
+
+    public void setState(final boolean value) {
+        this.value = value;
+    }
+
     public void toggle() {
         this.value = !this.value;
     }
